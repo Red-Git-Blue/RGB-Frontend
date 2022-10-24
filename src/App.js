c9b3018e03b418c0aab7025e7cd3f5ce4dc37b01
eternrust <eternrust@dsm.hs.kr>
2022-10-24 23:12:59 +0900

    [디자인 수정에 따른 페이지 수정]의 추가 사항들

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginView from "./Pages/Join/Login";
 import SignUpView from "./Pages/Join/Signup";
 import ErrorPage from "./Pages/ErrorPage";
+import Header from "./Pages/Header";
 import Main from "./Pages/Main";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+      <Header />
         <Routes>
           <Route path='/' element={<Main />}></Route>
           <Route path='/login' element={<LoginView />}></Route>
--- src/styleds.js
+++ src/styleds.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { useNavigate } from 'react-router-dom';
 
-const Login_box = styled.div`
+export const Login_box = styled.div`
     display:flex;
     flex-direction: column;
     justify-content: space-around;
@@ -14,60 +14,72 @@ const Login_box = styled.div`
     height: 90vh;
     padding: 5vh 0;
 `
-const Title = styled.span`
-    background: linear-gradient(90deg, #00CEFF 0%, #0075FF 100%);
-    background-clip: text;
-    -webkit-background-clip: text;
-    text-fill-color: transparent;
-    -webkit-text-fill-color: transparent;
+export const Title = styled.span`
+    font-family: 'NanumGothic';
+    font-style: normal;
     font-weight: 700;
-    font-size: 40px;
-    line-height: 47px;
+    font-size: 24px;
+    line-height: 28px;
+
+    text-transform: uppercase;
+
+    color: #FFFFFF;
 `
-const Out_box = styled.div`
+export const Out_box = styled.div`
     display: flex;
     flex-direction: column;
+    margin-bottom: 40px;
 `
 
-const Sub_title = styled.span`
-    font-weight: 700;
-    font-size: 20px;
-    line-height: 23px;
-    margin: 25px 0 15px 0;
+export const Sub_title = styled.span`
+    font-family: 'NanumGothic';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 18px;
+
     color: #fff;
+
     text-align: left;
+    margin: 0 0 13px 24px;
 `
 
-const Input_box = styled.input`
-    height: 60px;
-    width: 360px;
-    outline: none;
-    background: url(input-login.png);
-    background-size: cover;
+export const Input_box = styled.input`
+    width: 382px;
+    height: 40px;
+
+    background: transparent;
+    border: 1px solid #FFFFFF;
     border-radius: 64px;
-    border:none;
-    padding: 0 20px 0 20px;
-    font-weight: 400;
-    font-size: 18px;
-    line-height: 21px;
+    padding: 0 24px 0 24px;
+
+    font-family: 'NanumGothic';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 15px;
+    line-height: 17px;
     color: #fff;
+
     text-align: left;
     margin-bottom: 10px;
 `
 
-const Button_box = styled.button`
-    height: 60px;
-    width: 400px;
+export const Button_box = styled.button`
+    width: 430px;
+    height: 50px;
     outline: none;
     border-radius: 64px;
-    border:none;
-    background: linear-gradient(90deg, #00CEFF 0%, #0075FF 100%);
-    margin-top: 17px;
+    border: 1px solid #ffffff;
+    background: ${(props) => props.background || '#FFFFFF'};
+    color: ${(props) => props.color || '#000000'};
+    margin-top: ${(props) => props.top || '20px'};;
+
+    font-family: 'NanumGothic';
+    font-style: normal;
     font-weight: 700;
-    font-size: 24px;
-    line-height: 28px;
-    z-index: 1;
-    color: #fff;
+    font-size: 18px;
+    line-height: 21px;
+    
     cursor: pointer;
     transition: 0.3s;
     &:hover {
@@ -75,30 +87,42 @@ const Button_box = styled.button`
     }
 `
 
-const Input_view = ({name}) => {
+export const Blur_box = styled.div`
+    position: absolute;
+    min-width: ${(props) => props.width || '1074px'};
+    min-height: ${(props) => props.height || '638px'};
+
+    background: rgba(0, 0, 0, 0.01);
+    backdrop-filter: blur(120px);
+
+    border-radius: 20px;
+`
+
+export const Input_view = ({ name, text }) => {
     return (
         <Out_box>
-        <Sub_title>{name}</Sub_title>
-        <Input_box type={'text'} placeholder={`${name}를 입력해주세요`}/>
+            <Sub_title>{name}</Sub_title>
+            <Input_box type={'text'} placeholder={text} />
         </Out_box>
     )
-} 
+}
 
-const Auth_page = ({string, name, move}) => {
+export const Auth_page = ({ string, name, move }) => {
     let navigate = useNavigate();
     return (
         <div style={{
             display: 'flex',
+            alignItems: 'center',
             marginTop: '20px'
         }}>
             <span style={{
                 fontFamily: 'NanumGothic',
                 fontStyle: 'normal',
                 fontWeight: '400',
-                fontSize: '18px',
-                lineHeight: '21px',
+                fontSize: '14px',
+                lineHeight: '16px',
                 color: '#fff',
-                marginRight: '10px'
+                marginRight: '20px'
             }}>{string}</span>
             <span style={{
                 fontFamily: 'NanumGothic',
@@ -106,78 +130,70 @@ const Auth_page = ({string, name, move}) => {
                 fontWeight: '600',
                 fontSize: '18px',
                 lineHeight: '21px',
-                background: 'linear-gradient(90deg, #00CEFF 0%, #0075FF 100%)',
-                backgroundClip: 'text',
-                webkitBackgroundClip: 'text',
-                textFillColor: 'transparent',
-                webkitTextFillColor: 'transparent',
+                color: '#FFF500',
                 cursor: 'pointer',
                 display: 'block',
-                zIndex: '3',
-                }} onClick={() => navigate(move)}>{name} &gt;</span>
+            }} onClick={() => navigate(move)}>{name} &gt;</span>
         </div>
     )
 }
 
-const Background_view = () => {
-    let navigate = useNavigate();
-    return (
-        <div style={{
-            height:'100vh',
-            width:'70vw',
-            minWidth: '325px',
-            backgroundImage: "linear-gradient(0deg, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url('background-1.png')",
-            backgroundSize: "cover",
-            display:'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            flexDirection: 'column',
-        }}>
-            <div style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                flexDirection: 'column',
-                cursor: 'pointer'
-            }} onClick={() => navigate('/')}>
-            <img src="./Logo.png" width={'200px'} height={'200px'} />
-            <span style={{
-                fontFamily: 'Roboto',
-                fontStyle: 'normal',
-                fontWeight: '900',
-                fontSize: '3rem',
-                lineHeight: '47px',
-                color: '#fff',
-            }}>WELCOME TO</span>
-            <span style={{
-                fontFamily: 'Roboto',
-                fontStyle: 'normal',
-                fontWeight: '400',
-                fontSize: '2.5rem',
-                lineHeight: '38px',
-                letterSpacing: '-0.04em',
-                background: 'linear-gradient(90deg, #00CEFF 0%, #0075FF 100%)',
-                backgroundClip: 'text',
-                webkitBackgroundClip: 'text',
-                textFillColor: 'transparent',
-                webkitTextFillColor: 'transparent',
-            }}>STUDY OF STOCKS</span>
-            </div>
-        </div>
-    );
-}
+export const Background_view = styled.div`
+    width: 100%;
+    height: 100%;
+    background: url('background.png');
+    background-size: cover;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: -1;
+`
 
-const Ls_btn = ({click, string, name, sub_name, move}) => {
+export const Ls_btn = ({ click, string, name, sub_name, move }) => {
     return (
         <div style={{
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center'
         }}>
-            <Auth_page string={string} name={name} move={move}/>
+            <Auth_page string={string} name={name} move={move} />
             <Button_box onClick={() => click()}>{sub_name}</Button_box>
         </div>
     )
 }
 
-export { Background_view, Input_view, Login_box, Title, Out_box, Sub_title, Input_box, Button_box , Auth_page, Ls_btn};
+export const Left_box = ({title, text1, text2, text3, text4, link}) => {
+    return (
+        <Set_box>
+            <Text bottom='30px'>{title}</Text>
+            <Text bottom='10px'>{text1}</Text>
+            <Text weight='400' size='18px' height='21px' bottom='60px'>{text2}</Text>
+            <Auth_page string={text3} name={text4} move={link} />
+        </Set_box>
+    )
+}
+
+export const Right_box = styled.div`
+    width: 430px;
+    display: flex;
+    flex-direction: column;
+`
+
+const Set_box = styled.div`
+    font-family: 'NanumGothic';
+    font-style: normal;
+    display: flex;
+    flex-direction: column;
+    width: 444px;
+`
+
+const Text = styled.span`
+    font-weight: ${(props) => props.weight || '700'};;
+    font-size: ${(props) => props.size || '24px'};
+    line-height: ${(props) => props.height || '28px'};
+    margin-bottom: ${(props) => props.bottom || '0px'};
+
+    text-transform: uppercase;
+
+    color: #FFFFFF;
+`
