3c185cbf131e10290db811148e28ed640757fae4
eternrust <eternrust@dsm.hs.kr>
2022-12-03 23:29:47 +0900

    EDIT :: Coin페이지 chart 틀

--- src/Pages/User/Coin/index.jsx
+++ src/Pages/User/Coin/index.jsx
@@ -1,9 +1,23 @@
 import { useState } from "react";
 import styled from "styled-components";
+import { Image } from "../../../styleds";
+import { Doughnut } from 'react-chartjs-2';
+import Chart from "chart.js/auto";
 
+  const DATA_COUNT = 5;
+  const NUMBER_CFG = {count: DATA_COUNT, min: 0, max: 100};
+
+  const data = {
+    labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: [1, 2, 3, 4, 5]
+      }
+    ]
+  }
 const CoinView = () => {
     const [Btn_click, Set_click] = useState(1);
-
     return (
         <Out_box>
             {Btn_click === 1 ?
@@ -26,19 +40,21 @@ const CoinView = () => {
                                 <span>+2,429,371 (+53%)</span>
                             </div>
                             <div>
-                                <My_coin_detail_box bool={false}></My_coin_detail_box>
-                                <My_coin_detail_box bool={true}></My_coin_detail_box>
-                                <My_coin_detail_box bool={false}></My_coin_detail_box>
-                                <My_coin_detail_box bool={false}></My_coin_detail_box>
-                                <My_coin_detail_box bool={false}></My_coin_detail_box>
+                                <MyCoinDetail />
+                                <MyCoinDetail bool={true} />
+                                <MyCoinDetail />
+                                <MyCoinDetail />
+                                <MyCoinDetail />
+                                <MyCoinDetail />
+                                <MyCoinDetail />
+                                <MyCoinDetail />
                             </div>
                         </div>
                         <div>
-
+                            <Doughnut data={data}/>
                         </div>
                     </My_coin_box>
                     <AuthCoin_box>
-
                     </AuthCoin_box>
                 </>
                 :
@@ -55,6 +71,7 @@ const CoinView = () => {
                     </AuthPage_btn_box>
                 </>
             }
+            
         </Out_box>
     )
 }
@@ -106,6 +123,7 @@ const My_coin_box = styled.div`
     font-size: 20px;
     line-height: 23px;
     margin: 30px 0 72px 0;
+    display: flex;
     & > div {
         display: flex;
         justify-content: space-between;
@@ -139,6 +157,7 @@ const My_coin_box = styled.div`
                 width: 918px;
                 height: 740px;
                 border-radius: 20px;
+                overflow: scroll;
             }
         }
         &:last-child {
@@ -147,19 +166,96 @@ const My_coin_box = styled.div`
     }
 `
 
-const My_coin_detail_box = styled.div`
-    width: 918px;
-    height: 150px;
+const MyCoinDetail = ({ bool = false }) => {
+    return (
+        <MyCoinDetailBox bool={bool}>
+            <MyCoinDetailUserBox color="#0094FF">
+                <div>
+                    <Image src='/image/Profile.jpg' width='30px' height='30px' radius='30px' />
+                    <span>HYUNSUK</span>
+                </div>
+                <span>80.00%</span>
+                <span>평가액 146,291원</span>
+            </MyCoinDetailUserBox>
+            <MyCoinDetailChartBox>
+                <Image src='/image/TestChart.png' width='420px' height='90px' />
+                <div>
+                    <span>1,492</span>
+                    <span>+320 (+21%)</span>
+                </div>
+            </MyCoinDetailChartBox>
+        </MyCoinDetailBox>
+    )
+}
+
+const MyCoinDetailBox = styled.div`
+    width: 888px;
+    height: 120px;
     margin-bottom: 20px;
     background: #001708;
     border-radius: 20px;
+    padding: 15px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 16px;
+    color: #ffffff;
+
     ${(props) => props.bool && `
-        width: 910px;
-        height: 142px;
+        width: 880px;
+        height: 112px;
         background: #000E18;
         border: 4px solid #0094FF;
         box-shadow: 0px 0px 100px rgba(0, 148, 255, 0.5);
     `
+    }
+`
+
+const MyCoinDetailUserBox = styled.div`
+    margin-left: 25px;
+    display: flex;
+    flex-direction: column;
+    & > :nth-child(1) {
+        display: flex;
+        align-items: center;
+        & > :first-child{
+            margin-right: 10px;
+        }
+    }
+    & > :nth-child(2) {
+        margin: 6px 0;
+        font-weight: 700;
+        font-size: 24px;
+        line-height: 28px;
+    }
+    & > :nth-child(3) {
+        color: ${(props) => props.color || '#ffffff'}
+    }
+`
+
+const MyCoinDetailChartBox = styled.div`
+    width: 550px;
+    height: 120px;
+    background: linear-gradient(270deg, #000000 0%, rgba(0, 23, 22, 0) 100%);
+    border-radius: 20px;
+    padding: 0 30px 0 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    & > div:last-child {
+        display: flex;
+        flex-direction: column;
+        text-align: right;
+        & > span:last-child {
+            font-size: 12px;
+            line-height: 14px;
+            margin-top: 10px;
+            color: ${(props) => props.color || '#ff0000'}
+        }
     }
 `
 
