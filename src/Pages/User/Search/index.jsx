f78d9d75d271eb3e70644dd624694084afb76772
eternrust <eternrust@dsm.hs.kr>
2022-12-15 13:41:09 +0900

    EDIT :: Search 페이지 완성 및 각종 수정

--- src/Pages/User/Search/index.jsx
+++ src/Pages/User/Search/index.jsx
@@ -3,13 +3,34 @@ import { useCookies } from "react-cookie";
 import { useState } from "react";
 import styled from "styled-components";
 import {BaseUrl} from "../../../export/baseUrl";
+import { Image } from "../../../styleds";
+import { useNavigate } from "react-router";
 
 const SearchView = () => {
-    const [Btn_click, Set_click] = useState(1);
+    const [BtnClick, SetClick] = useState(1);
     const [cookies, , ] = useCookies(['accessToken']);
+    const [userData, setUser] = useState([]);
+    const [CoinData, setCoin] = useState([]);
 
     const Search = (e) => {
         if(!cookies.accessToken) return;
+        
+        axios({
+            method: 'get',
+            url: BaseUrl + '/coin/search',
+            headers: {
+                Authorization: `Bearer ${cookies.accessToken}`,
+            },
+            params: {
+                query: e.target.value.trim(),
+                idx: 0,
+                size: 10
+            }
+        })
+        .then((res) => {
+            setCoin(res.data);
+        })
+
         axios({
             method: 'get',
             url: BaseUrl + '/user/search',
@@ -17,44 +38,49 @@ const SearchView = () => {
                 Authorization: `Bearer ${cookies.accessToken}`,
             },
             params: {
-                q: e.target.value.trim(),
+                query: e.target.value.trim(),
                 idx: 0,
                 size: 10
             }
         })
         .then((res) => {
-            console.log(res.data);
-        })
-        .catch((err) => {
-            console.log(err);
+            setUser(res.data);
         })
     }
+
     return (
         <OutBox>
             <SearchInput onKeyDown={(e) => {if(e.key === 'Enter') Search(e)}}/>
-            {Btn_click === 1 ?
-                <AuthPage_btn_box>
-                    <AuthPage_btn
-                        background='#FFF500'
-                        color="#000000"
-                        onClick={(() => Set_click(-Btn_click))}
-                    >
-                        코인
-                    </AuthPage_btn>
-                    <AuthPage_btn onClick={(() => Set_click(-Btn_click))} >순위</AuthPage_btn>
-                </AuthPage_btn_box>
-                :
-                <AuthPage_btn_box>
-                    <AuthPage_btn onClick={(() => Set_click(-Btn_click))}>코인</AuthPage_btn>
-                    <AuthPage_btn
-                        background='#FFF500'
-                        color="#000000"
-                        onClick={(() => Set_click(-Btn_click))}
-                    >
-                        순위
-                    </AuthPage_btn>
-                </AuthPage_btn_box>
-            }
+            <AuthPage_btn_box>
+                <AuthPage_btn
+                    bool={BtnClick === 1}
+                    onClick={(() => BtnClick === -1 && SetClick(-BtnClick))}
+                >
+                    코인
+                </AuthPage_btn>
+                <AuthPage_btn
+                    bool={BtnClick === -1}
+                    onClick={(() => BtnClick === 1 && SetClick(-BtnClick))}
+                >
+                    순위
+                </AuthPage_btn>
+            </AuthPage_btn_box>
+            <GridBox>
+                {
+                    BtnClick === 1 ?
+                    <>
+                    {
+                        CoinData.map((item, index) => <Result key={index} type={1} data={item}/>)
+                    }
+                    </>
+                    :
+                    <>
+                    {
+                        userData.map((item, index) => <Result key={index} type={-1} data={item}/>)
+                    }
+                    </>
+                }
+            </GridBox>
         </OutBox>
     )
 }
@@ -99,8 +125,8 @@ const AuthPage_btn = styled.div`
     width: 77px;
     height: 40px;
     border-radius: 100px;
-    background: ${(props) => props.background || '#222222'};
-    color: ${(props) => props.color || '#999999'};
+    background: #222222;
+    color: #999999;
     font-family: 'Roboto';
     font-style: normal;
     font-weight: 700;
@@ -111,7 +137,141 @@ const AuthPage_btn = styled.div`
     justify-content: center;
     align-items: center;
     transition: 0.2s;
+
+    ${(props) => props.bool && `
+        background: #FFF500;
+        color: #000000;
+    `}
+
     &:hover {
         transform: scale(1.05);
     }
+`
+
+const GridBox = styled.div`
+    width: 1150px;
+    height: auto;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(370px, auto));
+    gap: 20px;
+`
+
+const Result = ({type, data}) => {
+    const Navigate = useNavigate();
+
+    return (
+        <ResultBox onClick={() => type === 1 ? Navigate('/coin/' + data.id) : Navigate('/userPage/' + data.id)}>
+            <div>
+                {
+                    data.profile? 
+                    <Image src={data.profile.fileUrl} width='35px' height='35px' radius='20px'/>
+                    :
+                    <EmptyImage />
+                }
+                <div>
+                    <span>{data.nickName}</span>
+                    <span>{data.name}</span>
+                </div>
+            </div>
+            {
+                type === 1 ?
+                <CoinBox color={data.increment.includes('-') === true ? "#0038FF" : "#FF0000"}>
+                    <span>{data.price}</span>
+                    <span>{data.increment.includes('-') ? data.increment : `+${data.increment}`}</span>
+                </CoinBox>
+                :
+                <BadgeBox>
+                    {
+                        data.representBadge &&
+                        <Image src={data.representBadge.fileUrl} width='70px' height='75px' radius='20px'/>
+                    }
+                </BadgeBox>
+            }
+        </ResultBox>
+    )
+}
+
+const ResultBox = styled.div`
+    width: 370px;
+    height: 75px;
+    background: #222222;
+    border-radius: 20px;
+    
+    display: flex;
+    justify-content: space-between;
+
+    cursor: pointer;
+    transition: 0.3s;
+
+    &:hover {
+        transform: scale(1.05);
+    }
+
+    & > div {
+        &:first-child {
+            margin: 20px 0 20px 20px;
+            display: flex;
+            & > div {
+                display: flex;
+                flex-direction: column;
+                margin-left: 10px;
+                font-family: 'Roboto';
+                font-style: normal;
+
+                & > :first-child {
+                    font-weight: 900;
+                    font-size: 14px;
+                    line-height: 16px;
+                    color: #FFFFFF;
+                    margin-bottom: 5px;
+                }
+
+                & > :last-child {
+                    font-weight: 400;
+                    font-size: 12px;
+                    line-height: 14px;
+                    color: #909090;
+                }
+            }
+        }
+    }
+`
+
+const EmptyImage = styled.div`
+    width: 34px;
+    height: 34px;
+    background: transparent;
+    border: 1px solid #ffffff;
+    border-radius: 20px;
+`
+
+const CoinBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    text-align: right;
+    margin: 20px 20px 20px 0;
+    font-family: 'Roboto';
+    font-style: normal;
+    & > :first-child {
+        font-weight: 700;
+        font-size: 14px;
+        line-height: 16px;
+        color: #FFFFFF;
+        margin-bottom: 5px;
+    }
+
+    &:last-child {
+        font-weight: 400;
+        font-size: 12px;
+        line-height: 14px;
+        color: ${(props) => props.color};
+    }
+`
+
+const BadgeBox = styled.div`
+    width: 70px;
+    height: 75px;
+    background-color: #111111;
+    backdrop-filter: blur(120px);
+    border-radius: 20px;
 `
